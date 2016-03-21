96bb9324be7ac81dd04ddd55247fb51b0f531f10
Axel Malmberg <axel_malmberg@hotmail.com>
2016-03-21 19:49:33 +0100

    connection is completed, verifying login completed, switcharo completed, thread made so that we can access internet. failed alot of attempts until we finished very easy attempt time taken, 7h x 3
    retry

--- app/src/main/java/com/example/albinskola/fitnessproject/DbConnecter.java
+++ app/src/main/java/com/example/albinskola/fitnessproject/DbConnecter.java
@@ -1,13 +1,52 @@
 package com.example.albinskola.fitnessproject;
 
+    import android.net.NetworkInfo;
+    import android.os.AsyncTask;
+    import android.os.StrictMode;
+
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.ResultSet;
+    import java.sql.SQLException;
+    import java.sql.Statement;
+    import java.util.ArrayList;
+
 public class DbConnecter {
+    Connection con = null;
+    String url = "jdbc:mysql://185.76.64.28:3306/etvffqgz_FitnessBass";
+    String usr = "etvffqgz_admin";
+    String pass = "admin";
 
 
-    public void getSomething(){
 
+    public int verifyLogin(String userName,String password) {
+        try {
+
+            System.out.println(userName + " " + password);
+            Class.forName("com.mysql.jdbc.Driver");
+
+            con = DriverManager.getConnection(url, usr, pass);
+
+            Statement st = con.createStatement();
+
+            ResultSet rs = st.executeQuery("SELECT * FROM Users");
+            System.out.println("1");
+            while(rs.next()) {
+                String tmpUser = rs.getString("UserName");
+                String tmpPassword = rs.getString("Password");
+                if (tmpUser.equals(userName) && tmpPassword.equals(password)) {
+                    return 2;
+                }
+            }
+
+
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+
+
+        return 1;
     }
-
-
-
-
 }
--- app/src/main/java/com/example/albinskola/fitnessproject/MainActivity.java
+++ app/src/main/java/com/example/albinskola/fitnessproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.albinskola.fitnessproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hej");
+        startActivity(new Intent(MainActivity.this, LoginHandler.class));
 }
 }
