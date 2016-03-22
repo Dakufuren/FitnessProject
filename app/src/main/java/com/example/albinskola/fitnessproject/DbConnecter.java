60d726a232a156ffc4bafd433dfcd22b134ed4e2
Axel Malmberg <axel_malmberg@hotmail.com>
2016-03-22 15:52:34 +0100

    Added addWorkout method in the DbConnector

--- app/src/main/java/com/example/albinskola/fitnessproject/DbConnecter.java
+++ app/src/main/java/com/example/albinskola/fitnessproject/DbConnecter.java
@@ -10,12 +10,15 @@ package com.example.albinskola.fitnessproject;
     import java.sql.SQLException;
     import java.sql.Statement;
     import java.util.ArrayList;
+    import java.util.Date;
 
 public class DbConnecter {
     Connection con = null;
     String url = "jdbc:mysql://185.76.64.28:3306/etvffqgz_FitnessBass";
     String usr = "etvffqgz_admin";
     String pass = "admin";
+
+    String USERNAME = "test";
 
 
     public void connect() {
@@ -56,4 +59,24 @@ public class DbConnecter {
 
         return 1;
     }
+
+    public void addWorkOut(Date date, int elapsedTime, int intensity, String description, int biceps, int triceps, int shoulders, int traps,
+                           int upperBack, int lowerBack, int chest, int abdomen, int glutes, int hamstrings, int quadriceps, int calves) {
+
+        try {
+            connect();
+
+            Statement st = con.createStatement();
+
+            String sqlWorkout = String.format("INSERT INTO Workouts (Users_UserName, TheDate, ElapsedTime, Intensity, Note, Biceps, Triceps, Shoulders, Traps, UpperBack, LowerBack, Chest, Abdomen, Glutes, Hamstrings, Quadriceps, Calves) VALUES ('%s', '%s', %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d)", USERNAME, date, elapsedTime, intensity, description, biceps, triceps, shoulders, traps,
+            upperBack, lowerBack, chest, abdomen, glutes, hamstrings, quadriceps, calves);
+
+            st.executeUpdate(sqlWorkout);
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+    }
 }
+
