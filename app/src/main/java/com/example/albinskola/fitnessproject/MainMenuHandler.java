fc5232c2f92539c2aa79599ee7284c0e401ef401
Albin Åberg <albin.aberg@outlook.com>
2016-03-22 17:39:22 +0100

    Commit foshomannen Fixed some in mainmenuhandler

--- app/src/main/java/com/example/albinskola/fitnessproject/MainMenuHandler.java
+++ app/src/main/java/com/example/albinskola/fitnessproject/MainMenuHandler.java
@@ -1,5 +1,6 @@
 package com.example.albinskola.fitnessproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,5 +20,55 @@ public class MainMenuHandler extends AppCompatActivity {
 
 
     }
+
+    public void onClickAdd(View v){
+        System.out.println("Add Clicked!!!");
+        Intent intent = new Intent(MainMenuHandler.this, AddWorkoutHandler.class);
+        startActivity(intent);
+    }
+
+    public void onClickView(View v){
+        System.out.println("View Clicked!!!");
+        /*
+        Intent intent = new Intent(MainMenuHandler.this, ViewWorkoutHandler.class);
+        startActivity(intent);
+        */
+    }
+
+    public void onClickProfile(View v){
+        System.out.println("Profile Clicked!!!");
+        /*
+        Intent intent = new Intent(MainMenuHandler.this, ViewWorkoutHandler.class);
+        startActivity(intent);
+        */
+    }
+
+    public void onClickStats(View v){
+        System.out.println("Stats Clicked!!!");
+        /*
+        Intent intent = new Intent(MainMenuHandler.this, ViewWorkoutHandler.class);
+        startActivity(intent);
+        */
+    }
+
+    public void onClickTips(View v){
+        System.out.println("Tips Clicked!!!");
+        /*
+        Intent intent = new Intent(MainMenuHandler.this, ViewWorkoutHandler.class);
+        startActivity(intent);
+        */
+
+    }
+
+    public void onClickLeftover(View v){
+        System.out.println("Leftover Clicked!!!");
+        /*
+        Intent intent = new Intent(MainMenuHandler.this, ViewWorkoutHandler.class);
+        startActivity(intent);
+        */
+
+    }
+
+
 
 }
