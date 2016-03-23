de6c10f3568406f384908e34355e58870fa92533
Axel Malmberg <axel_malmberg@hotmail.com>
2016-03-23 16:48:30 +0100

    added ProfileHandler class and XML

---
+++ app/src/main/java/com/example/albinskola/fitnessproject/ProfileHandler.java
@@ -0,0 +1,13 @@
+package com.example.albinskola.fitnessproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileHandler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_handler);
+    }
+}
