07896f8c1db933b58edddc3aa905859c38b726e6
Albin Åberg <albin.aberg@outlook.com>
2016-03-24 17:14:21 +0100

    Started coding in viewWorkoutHandler

--- app/src/main/java/com/example/albinskola/fitnessproject/ViewWorkoutHandler.java
+++ app/src/main/java/com/example/albinskola/fitnessproject/ViewWorkoutHandler.java
@@ -1,19 +1,53 @@
 package com.example.albinskola.fitnessproject;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ViewWorkoutHandler extends AppCompatActivity {
 
     private ListView fitnessList;
+    private TextView tv;
+    private CalendarView cv;
+    DbConnecter db = new DbConnecter();
+    public static final String PREFS_NAME = "shared_pref";
+    SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_workout_handler);
 
-        fitnessList = (ListView) findViewById(R.id.listView);
+        pref = getApplicationContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
 
+        //fitnessList = (ListView) findViewById(R.id.listView);
+        tv = (TextView) findViewById(R.id.textView);
+        //cv = (CalendarView) findViewById(R.id.calendarView);
+
+
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String userName = pref.getString("username", null);
+
+                ArrayList<Workout> ls = new ArrayList<>();
+                ls = db.getWorkOuts(userName);
+                System.out.println(ls.toString());
+                System.out.println(ls.get(0).getBiceps());
+                System.out.println(ls.get(1).getBiceps());
+
+
+
+                //tv.setText();
+            }
+        });
+        thread1.start();
     }
 }
