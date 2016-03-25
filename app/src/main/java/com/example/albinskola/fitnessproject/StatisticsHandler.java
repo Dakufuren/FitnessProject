608ac71d20bc09da98974b8e0d38adaf87b55b08
Axel Malmberg <axel_malmberg@hotmail.com>
2016-03-25 16:05:31 +0100

    minor changes

--- app/src/main/java/com/example/albinskola/fitnessproject/StatisticsHandler.java
+++ app/src/main/java/com/example/albinskola/fitnessproject/StatisticsHandler.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
@@ -20,12 +21,19 @@ public class StatisticsHandler extends AppCompatActivity {
 
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<DataPoint>(new DataPoint[] {
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
                 new DataPoint(3, 2),
-                new DataPoint(4, 6)
+                new DataPoint(4, 6),
+                new DataPoint(5, 10),
+                new DataPoint(6, 4),
+                new DataPoint(7, 18),
+                new DataPoint(8, 4),
+                new DataPoint(9, 12),
+                new DataPoint(10, 1),
+                new DataPoint(11, 2)
         });
         graph.addSeries(series);
 
