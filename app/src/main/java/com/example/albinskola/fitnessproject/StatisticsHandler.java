2e4c439525ba955ee3e45e1af564f4ee4aa66019
Axel Malmberg <axel_malmberg@hotmail.com>
2016-03-25 17:08:29 +0100

    more changes in stats

--- app/src/main/java/com/example/albinskola/fitnessproject/StatisticsHandler.java
+++ app/src/main/java/com/example/albinskola/fitnessproject/StatisticsHandler.java
@@ -8,16 +8,39 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class StatisticsHandler extends AppCompatActivity {
+    int bicepsCounter = 0;
+    int tricepsCounter = 0;
+    int shouldersCounter = 0;
+    int trapsCounter = 0;
+    int upperbackCounter = 0;
+    int lowerbackCounter = 0;
+    int chestCounter = 0;
+    int abdomenCounter = 0;
+    int glutesCounter = 0;
+    int hamstringsCounter = 0;
+    int quadricepsCounter = 0;
+    int calvesCounter = 0;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics_handler);
+
+
+
+
+
 
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
@@ -35,8 +58,20 @@ public class StatisticsHandler extends AppCompatActivity {
                 new DataPoint(10, 1),
                 new DataPoint(11, 2)
         });
+        series.setSpacing(50);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMaxX(11);
+        graph.getLegendRenderer().setVisible(true);
+
+        graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+
         graph.addSeries(series);
 
+        /*
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setHorizontalLabels(new String[]{"biceps", "triceps", "shoudlers", "traps", "upper Back", "lower back", "chest", "abdomen", "glutes", "hamstrings", "quadriceps", "calves"});
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);*/
 
     }
 
