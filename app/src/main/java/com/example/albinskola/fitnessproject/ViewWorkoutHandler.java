baf217fa7989778a8a3f4d5d7ce41b18a3194bbf
Albin Åberg <albin.aberg@outlook.com>
2016-03-25 17:11:00 +0100

    ViewWorkoutHandler is new viewing the dates of the objects. TODO: Add clickable with popup message containing the data.

--- app/src/main/java/com/example/albinskola/fitnessproject/ViewWorkoutHandler.java
+++ app/src/main/java/com/example/albinskola/fitnessproject/ViewWorkoutHandler.java
@@ -4,20 +4,29 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ViewWorkoutHandler extends AppCompatActivity {
 
     private ListView fitnessList;
     private TextView tv;
     private CalendarView cv;
+
     DbConnecter db = new DbConnecter();
     public static final String PREFS_NAME = "shared_pref";
     SharedPreferences pref;
+    List<Workout> nls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,7 @@ public class ViewWorkoutHandler extends AppCompatActivity {
 
         //fitnessList = (ListView) findViewById(R.id.listView);
         tv = (TextView) findViewById(R.id.textView);
+        fitnessList = (ListView) findViewById(R.id.listView);
         //cv = (CalendarView) findViewById(R.id.calendarView);
 
 
@@ -39,15 +49,49 @@ public class ViewWorkoutHandler extends AppCompatActivity {
 
                 ArrayList<Workout> ls = new ArrayList<>();
                 ls = db.getWorkOuts(userName);
-                System.out.println(ls.toString());
-                System.out.println(ls.get(0).getBiceps());
-                System.out.println(ls.get(1).getBiceps());
 
+                nls = Collections.synchronizedList(new ArrayList<Workout>());
+                nls = ls;
 
-
-                //tv.setText();
             }
         });
         thread1.start();
+
+        while (nls == null) {
+
+        }
+        if (nls != null) {
+            thread1.interrupt();
+            System.out.println(nls.get(0).getDate());
+
+            String[] strTemp;
+            strTemp = new String[nls.size()];
+
+            for(int i = 0; i < nls.size(); i++){
+                java.util.Date tempDate;
+                tempDate = nls.get(i).getDate();
+
+                Format formatter = new SimpleDateFormat("yyyy-MM-dd");
+                String temp = formatter.format(tempDate);
+
+                strTemp[i] = temp;
+            }
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+                    R.layout.listview,
+                    strTemp);
+            fitnessList.setAdapter(adapter);
+
+            fitnessList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+            {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    long id1 = id;
+
+                }
+            });
+        }
+
+
     }
 }
